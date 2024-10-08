1ef1956daf87cb8ed4a1d1b7c34540839ed370a1
Sliwno <evg.gordo@gmail.com>
2024-10-08 16:16:49 +0200

    Find icon Funktion überarbeitet, damit das Programm auch ohne Icons laufen kann

--- src/PlzValidator.py
+++ src/PlzValidator.py
@@ -2,11 +2,11 @@ import sys
 import logging
 import os
 import csv
+from datetime import datetime
+import sqlite3
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QRadioButton, QLabel, QDialog, QLineEdit, QMessageBox
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize
-import sqlite3
-
 
 # Konfigurieren des Loggings
 logging.basicConfig(level=logging.ERROR,  # Setzt das Log-Level auf ERROR
@@ -16,15 +16,18 @@ class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("CSV Import/Export")
-        self.setFixedSize(300, 170)  # Fenstergröße
+       
+        # Fenstergröße
+        self.setFixedSize(300, 170)  
+
         # Dynamische Pfadsetzung für das Icon
         try:
             window_icon_path = self.find_icon('validierung.png')
             self.setWindowIcon(QIcon(window_icon_path))
         except FileNotFoundError as e:
             print(e)
-
-        # Layout
+            
+        # Layout an dem die Widgets von oben nach unten angeordnet werden
         layout = QVBoxLayout()
 
         # Initialisiere die Datenbank
@@ -59,14 +62,20 @@ class MyWindow(QWidget):
             self.settings_button.setIcon(QIcon(settings_icon_path))
         except FileNotFoundError as e:
             print(e)
-        self.settings_button.setIcon(QIcon(settings_icon_path))
-        self.settings_button.setIconSize(QSize(16, 16))  # Hier kannst du die Größe des Icons festlegen
+
+        if settings_icon_path:    
+            self.settings_button.setIcon(QIcon(settings_icon_path))
+            self.settings_button.setIconSize(QSize(16, 16))  # Hier kannst du die Größe des Icons festlegen
+        else:
+            self.settings_button.setText("Einstellungen")
+
         self.settings_button.clicked.connect(self.show_settings)
         self.settings_button.setFixedSize(20, 20)
 
         # Standardmäßig die Blacklist auswählen
         self.blacklist_radio.setChecked(True)
 
+        # Füge die Radio-Buttons und den Einstellungsbutton zum Layout hinzu
         radio_layout.addWidget(self.whitelist_radio)
         radio_layout.addWidget(self.blacklist_radio)
         radio_layout.addWidget(self.settings_button)
@@ -82,42 +91,39 @@ class MyWindow(QWidget):
         self.setLayout(layout)
 
     def find_icon(self, filename):
-        # Aktuelles Verzeichnis
-        current_directory = os.path.dirname(os.path.abspath(__file__))
+        directory = os.walk(os.getcwd())
+        for root, _, files in directory:
+            for file in files:
+                if file == filename:
+                    return os.path.join(root, file)
         
-        # Elternverzeichnis
-        parent_directory = os.path.dirname(current_directory)
-        
-        # Pfade im aktuellen und Elternverzeichnis
-        current_path = os.path.join(current_directory, 'data', filename)
-        parent_path = os.path.join(parent_directory, 'data', filename)
-        
-        # Prüfe, ob das Icon im aktuellen Verzeichnis existiert
-        if os.path.exists(current_path):
-            self.current_path
-        # Prüfe, ob das Icon im Elternverzeichnis existiert
-        elif os.path.exists(parent_path):
-            self.parent_path
-        else:
-            raise FileNotFoundError(f"Icon '{filename}' nicht gefunden.")
-
+            
     def import_csv_vorlage(self):
         try:
             # Öffnet einen Datei-Dialog zum Auswählen der ersten CSV-Datei
             file_dialog = QFileDialog(self)
-            file_path, _ = file_dialog.getOpenFileName(self, "CSV 1 auswählen", "", "CSV-Dateien (*.csv)")
+            file_path, _ = file_dialog.getOpenFileName(self, "CSV PLZ-Vorlage auswählen", "", "CSV-Dateien (*.csv)")
             self.label_btn1.setText(f"Pfad: {file_path}")
 
             if file_path:
                 # Hier kann der Code zum Einlesen der ersten CSV-Datei platziert werden
                 print(f"CSV 1 importiert: {file_path}")
                 reader = self.read_csv(file_path)
+                
                 self.vorlage_numbers = []
                 for item in reader:
-                    self.vorlage_numbers.append(int(item[0]))
+                    try:
+                        self.vorlage_numbers.append(int(item[0]))
+                
+                    except ValueError as ve:
+                        error_message = f"Fehler beim Verarbeiten des Eintrags '{item[0]}'\nERROR: {ve} \n\nVerarbeitung wird fortgesetzt."
+                        logging.error(error_message)
+                        QMessageBox.critical(self,"Verarbeitungsfehler",error_message)  # Benutzerfeedback geben
+                        continue  # Weiter mit dem nächsten Eintrag
+
                 self.vorlage_numbers = list(set(self.vorlage_numbers))  # Duplikate entfernen
                 self.vorlage_numbers.sort()  # Liste sortieren
-                
+                    
         except FileNotFoundError:
             logging.error("Die ausgewählte Datei wurde nicht gefunden.")
             QMessageBox.critical(self, "Fehler", "Die ausgewählte Datei wurde nicht gefunden.")
@@ -134,7 +140,7 @@ class MyWindow(QWidget):
         try: 
             # Öffnet einen Datei-Dialog zum Auswählen der zweiten CSV-Datei
             file_dialog = QFileDialog(self)
-            file_path, _ = file_dialog.getOpenFileName(self, "CSV 2 auswählen", "", "CSV-Dateien (*.csv)")
+            file_path, _ = file_dialog.getOpenFileName(self, "CSV PLZ-Range auswählen", "", "CSV-Dateien (*.csv)")
             self.label_btn2.setText(f"Pfad: {file_path}")
 
             if file_path:
@@ -150,13 +156,12 @@ class MyWindow(QWidget):
                             start, end = map(int, item[0].split("-"))  # Split in Start und Ende
                             self.range_numbers.extend(range(start, end + 1))  # Den Bereich zur Liste hinzufügen
                         else:
-                            pass
                             # Wenn es sich um eine einzelne Zahl handelt, füge sie direkt hinzu
                             self.range_numbers.append(int(item[0]))
                     except ValueError as ve:
                         error_message = f"Fehler beim Verarbeiten des Eintrags '{item[0]}': {ve}"
                         logging.error(error_message)
-                        self.text_edit.append(error_message)  # Benutzerfeedback geben
+                        QMessageBox.critical(self,"Verarbeitungsfehler", f"{error_message}")  # Benutzerfeedback geben
                         continue  # Weiter mit dem nächsten Eintrag
 
                 self.range_numbers = list(set(self.range_numbers))  # Duplikate entfernen
@@ -165,57 +170,65 @@ class MyWindow(QWidget):
         except Exception as e:
             error_message = f"Fehler: {str(e)}"
             logging.error(error_message)
-            QMessageBox.critical(self, "Fehler", error_message)  # Benutzerfeedback geben
+            QMessageBox.critical(self, "Fehler: ", error_message)  # Benutzerfeedback geben
 
     def export_csv(self):
         try:   
             export_path = self.read_export_path()
             if export_path:
                 if self.whitelist_radio.isChecked():
-                    # Prüfen, ob beide CSV-Dateien importiert wurden
-                    if hasattr(self, 'vorlage_numbers') and hasattr(self, 'range_numbers'):
-                        # Filtere die Zahlen aus der Vorlage, die in der Range-Liste enthalten sind
-                        filtered_numbers = [num for num in self.vorlage_numbers 
-                                            if num in self.range_numbers]
-                        # Schreibe die gefilterten Zahlen in eine neue CSV-Datei
-                        with open(os.path.join(export_path, "filtered_plz.csv"),'w',newline='', encoding='utf-8') as csvfile:
-                            writer = csv.writer(csvfile)
-                            writer.writerow(["PLZ"]) # Schreibe die Spaltenüberschrift
-                            for num in filtered_numbers:
-                                writer.writerow([num])
-
-                        self.show_success_message("CSV-Datei erfolgreich erstellt!")
-                    
-                    else:
-                        logging.error("Fehler: CSV-Dateien nicht importiert.")
-                        self.text_edit.setPlainText("Fehler: CSV-Dateien nicht importiert.")
+                    self.whitelist_filter()
 
                 elif self.blacklist_radio.isChecked():
-                    # Prüfen, ob beide CSV-Dateien importiert wurden
-                    if hasattr(self, 'vorlage_numbers') and hasattr(self, 'range_numbers'):
-                        # Filtere die Zahlen aus der Vorlage, die nicht in der Range-Liste enthalten sind
-                        filtered_numbers = [num for num in self.vorlage_numbers 
-                                            if num not in self.range_numbers]
-                        # Schreibe die gefilterten Zahlen in eine neue CSV-Datei
-                        with open(os.path.join(export_path, "filtered_plz.csv"),'w',newline='', encoding='utf-8') as csvfile:
-                            writer = csv.writer(csvfile)
-                            writer.writerow(["PLZ"])  # Schreibe die Spaltenüberschrift
-                            for num in filtered_numbers:
-                                writer.writerow([num])
+                    self.blacklist_filter()
                         
-                        self.show_success_message("CSV-Datei erfolgreich erstellt!")
-                    
-                    else:
-                        logging.error("Fehler: CSV-Dateien nicht importiert.")
-                        self.text_edit.setPlainText("Fehler: CSV-Dateien nicht importiert.")
                 else:
                     logging.error("Fehler: Filtermethode nicht ausgewählt.")
-                    self.text_edit.setPlainText("Fehler: Filtermethode nicht ausgewählt.")
+                    QMessageBox.critical(self,f"Fehler", "Filtermethode nicht ausgewählt.")
+                
+                self.export_file(export_path)
             else:
                 logging.error("Exportpfad nicht festgelegt.")
-                self.text_edit.setPlainText("Fehler: Exportpfad nicht festgelegt.")   
+                QMessageBox.critical(self,f"Fehler", "Exportpfad nicht festgelegt.")   
+
         except Exception as e:
             QMessageBox.critical(self, "Fehler", f"Fehler beim Lesen der Datei: {str(e)}")
+
+    def whitelist_filter(self):
+        # Prüfen, ob beide CSV-Dateien importiert wurden
+        if hasattr(self, 'vorlage_numbers') and hasattr(self, 'range_numbers'):
+            # Filtere die Zahlen aus der Vorlage, die in der Range-Liste enthalten sind
+            self.filtered_numbers = [num for num in self.vorlage_numbers 
+                                if num in self.range_numbers]     
+        else:
+            logging.error("Fehler: CSV-Dateien nicht importiert.")
+            QMessageBox.critical(self, f"Fehler", "CSV-Dateien nicht importiert.")
+
+    def blacklist_filter(self):
+        # Prüfen, ob beide CSV-Dateien importiert wurden
+        if hasattr(self, 'vorlage_numbers') and hasattr(self, 'range_numbers'):
+            # Filtere die Zahlen aus der Vorlage, die nicht in der Range-Liste enthalten sind
+            self.filtered_numbers = [num for num in self.vorlage_numbers 
+                                if num not in self.range_numbers]
+        else:
+            logging.error("Fehler: CSV-Dateien nicht importiert.")
+            QMessageBox.critical(self,f"Fehler","CSV-Dateien nicht importiert.")
+
+    def export_file(self, export_path):
+
+        # Aktuelles Datum im Format 'YYYY-MM-DD'
+        current_date = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+        # Dateiname mit Datum
+        filename = f"filtered_plz_{current_date}.csv"
+        # Schreibe die gefilterten Zahlen in eine neue CSV-Datei
+        
+        with open(os.path.join(export_path, filename),'w',newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["PLZ"])  # Schreibe die Spaltenüberschrift
+            for num in self.filtered_numbers:
+                writer.writerow([num])
+            self.show_success_message("CSV-Datei erfolgreich erstellt!")
 
     def show_success_message(self, message):
         msg_box = QMessageBox()
@@ -241,14 +254,14 @@ class MyWindow(QWidget):
                 print("CSV_DATA ERFOLGREICH IMPORTIERT\n")
                 return csv_data
         except Exception as e:
-            self.text_edit.setPlainText(f"Fehler beim Lesen der Datei: {str(e)}")
+            QMessageBox.critical(self,"Fehler",f"beim Lesen der Datei: {str(e)}")
     
     def initialize_database(self):
-        # Aktuelles Verzeichnis ermitteln
-        current_directory = os.getcwd()
-        
         # Pfad zur Datenbank
-        db_path = os.path.join(current_directory, "settings.db")
+        db_path = self.find_db() 
+        if not db_path:
+            db_path = os.path.join(os.getcwd(), 'settings.db')
+            os.path.exists(db_path)
 
         # Verbindung zur Datenbank herstellen und die Tabelle "settings" erstellen, falls sie nicht existiert
         self.conn = sqlite3.connect(db_path)
@@ -260,6 +273,15 @@ class MyWindow(QWidget):
                             export_path TEXT
                         )''')
         self.conn.commit()
+
+    def find_db(self):
+        directory = os.walk(os.getcwd())
+        for root, _, files in directory:
+            print(root)
+            for file in files:
+                if file == ('settings.db'):
+                    return os.path.join(root, file)
+                
 
     def show_settings(self):
         # Erstelle ein neues Dialog-Fenster
@@ -335,7 +357,7 @@ class MyWindow(QWidget):
             self.conn.commit()
         dialog.accept()
 
-def main():
+def main():    
     app = QApplication(sys.argv)
     window = MyWindow()
     window.show()
